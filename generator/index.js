05a2a2e50750df05739c6d9e103d9ce4e5117044
Mikolaj Smolenski <m.smolenski@mdbootstrap.com>
2021-02-18 14:47:40 +0100

    Add pro installation option & welcome page config

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,7 @@
 const updateMainJS = require('./commands/updateMainJS')
 const applyRoboto = require('./commands/applyRoboto')
 const applyFA5 = require('./commands/applyFA5')
+const packTgz = require('./commands/packTgz')
 
 const remoteGitTags = require('remote-git-tags')
 
@@ -12,14 +13,26 @@ remoteGitTags('https://github.com/mdbootstrap/mdb-vue-ui-kit').then(tags => {
 
 module.exports = async (api, options) => {
   // extend existing config
-  api.extendPackage({
-    scripts: {
-      start: 'vue-cli-service serve --open'
-    },
-    dependencies: {
-      'mdb-vue-ui-kit': `^${latestTag}`
-    }
-  })
+  if (options.version === 'Free') {
+    api.extendPackage({
+      scripts: {
+        start: 'vue-cli-service serve --open'
+      },
+      dependencies: {
+        'mdb-vue-ui-kit': `^${latestTag}`
+      }
+    })
+  } else {
+    api.render('./templates/pro')
+    api.extendPackage({
+      scripts: {
+        start: 'vue-cli-service serve --open'
+      },
+      dependencies: {
+        'mdb-vue-ui-kit': `git+https://oauth2:${options.token}@git.mdbootstrap.com/mdb/vue/mdb5/prd/mdb5-vue-ui-kit-pro-essential.git`
+      }
+    })
+  }
 
   api.onCreateComplete(() => {
     updateMainJS(api.entryFile)
@@ -36,6 +49,18 @@ module.exports = async (api, options) => {
   if (options.fa5 === true) {
     api.onCreateComplete(() => {
       applyFA5()
+    })
+  }
+
+  // apply welcome page
+  if (options.welcomePage === true) {
+    api.render('./templates/welcomePage')
+  }
+
+  // npm pack & replace dependency by tgz
+  if (options.version === 'Pro') {
+    api.onCreateComplete(() => {
+      packTgz(latestTag, options.token)
     })
   }
 
--- prompts.js
+++ prompts.js
@@ -1,4 +1,21 @@
+const isPro = (answers) => {
+  return answers.version === 'Pro'
+}
+
 module.exports = [
+  {
+    name: 'version',
+    type: 'list',
+    message: 'Free or Pro version?',
+    choices: ['Free', 'Pro'],
+    default: 'Free'
+  },
+  {
+    name: 'token',
+    type: 'input',
+    message: 'Please enter your gitlab token:',
+    when: isPro
+  },
   {
     name: 'roboto',
     type: 'confirm',
@@ -10,5 +27,11 @@ module.exports = [
     type: 'confirm',
     message: 'Add Font Awesome 5?',
     default: true
-  }
+  },
+  {
+    name: 'welcomePage',
+    type: 'confirm',
+    message: 'Add MDB welcome page?',
+    default: true
+  },
 ]
